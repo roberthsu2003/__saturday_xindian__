099071521d57e33ae824ed828775ecb8ecd74081
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-19 20:04:30 +0800

    modify

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -1,9 +1,15 @@
 import tkinter as tk
 
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.title("這是第一個視窗")
+        self.geometry("600x400")
+
+
+
 def main():
-    window = tk.Tk()
-    window.title("這是第一個視窗")
-    window.geometry("600x400")
+    window = Window()
     window.mainloop()
 
 if __name__ == '__main__':
