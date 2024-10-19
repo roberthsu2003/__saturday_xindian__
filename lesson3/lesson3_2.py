2f58f29901808c878a737beafe14c97260adac92
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-19 21:57:22 +0800

    modify

--- lesson3/lesson3_2.py
+++ lesson3/lesson3_2.py
@@ -6,14 +6,17 @@ class Window(ThemedTk):
         super().__init__(*args, **kwargs)
         self.title("這是第一個視窗")
         self.geometry("600x400")
+        style = ttk.Style(self)
+        style.configure('TButton',font=('Ariel',15))
+
         btn1 = ttk.Button(self,text='按鈕1',command=self.user_click1)
-        btn1.pack()
+        btn1.pack(ipadx=20,ipady=10,pady=10,side='left')
 
         btn2 = ttk.Button(self,text='按鈕2',command=self.user_click2)
-        btn2.pack()
+        btn2.pack(ipadx=20,ipady=10,side='left')
 
         btn3 = ttk.Button(self,text='按鈕3',command=self.user_click3)
-        btn3.pack()
+        btn3.pack(ipadx=20,ipady=10,pady=10,side='left')
 
     def user_click1(self):
         print('使用者按下按鈕1')
