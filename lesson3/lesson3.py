54c93e32faa1862d8219ccdca05ff9a6d7f025fb
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-19 19:41:53 +0800

    modify

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title("這是第一個視窗")
-window.geometry("600x400")
-window.mainloop()
+def main():
+    window = tk.Tk()
+    window.title("這是第一個視窗")
+    window.geometry("600x400")
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
+    
