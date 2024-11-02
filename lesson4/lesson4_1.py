299f6d6e597fdb6b88b801afa283b73cdf9a0bce
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-02 19:18:19 +0800

    modify

---
+++ lesson4/lesson4_1.py
@@ -0,0 +1,32 @@
+from ttkthemes import ThemedTk
+from tkinter import ttk
+
+class Window(ThemedTk):
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("這是第一個視窗")
+        self.geometry("600x400")
+
+        style = ttk.Style(self)
+        
+        #style.configure('TLabel',font=('Ariel', 30),foreground='red') #修改內建的TLabel
+        #style.configure('Title.TLabel',font=('Ariel', 30),foreground='blue') #自訂的TLabel style
+
+
+        #label = ttk.Label(self,text='Hello! Tkinter!',style='Title.TLabel')
+        #label.pack()
+        topFrame = ttk.Frame(self,width=300,height=100)
+        topFrame.pack()
+
+        bottomFrame = ttk.Frame(self,width=400,height=200)
+        bottomFrame.pack()
+
+
+
+def main():
+    window = Window(theme="arc")
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
+    
