06848dd75e360c2b58df9b9095e2b473ece9f308
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-02 19:46:49 +0800

    modify

--- lesson4/lesson4_1.py
+++ lesson4/lesson4_1.py
@@ -5,20 +5,21 @@ class Window(ThemedTk):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("這是第一個視窗")
-        self.geometry("600x400")
+        #self.geometry("600x400")
 
         style = ttk.Style(self)
         
-        #style.configure('TLabel',font=('Ariel', 30),foreground='red') #修改內建的TLabel
-        #style.configure('Title.TLabel',font=('Ariel', 30),foreground='blue') #自訂的TLabel style
+        style.configure('TLabel',font=('Ariel', 30),foreground='red') #修改內建的TLabel
+        style.configure('Title.TLabel',font=('Ariel', 30),foreground='#000000') #自訂的TLabel style
 
 
-        #label = ttk.Label(self,text='Hello! Tkinter!',style='Title.TLabel')
-        #label.pack()
-        topFrame = ttk.Frame(self,width=300,height=100)
-        topFrame.pack()
+        
+        topFrame = ttk.Frame(self,borderwidth=2,relief='groove')
+        title_label = ttk.Label(topFrame,text='全台空氣品質AQI',style='Title.TLabel')
+        title_label.pack(pady=50,padx=20)
+        topFrame.pack(expand=True,fill='both',padx=10,pady=30)
 
-        bottomFrame = ttk.Frame(self,width=400,height=200)
+        bottomFrame = ttk.Frame(self)
         bottomFrame.pack()
 
 
