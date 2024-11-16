b1592e452d5cfb8e749ef3dee895579161219c31
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-16 20:41:08 +0800

    lesson5_5

--- lesson5/view/display.py
+++ lesson5/view/display.py
@@ -1,5 +1,7 @@
 from tkinter.simpledialog import Dialog
 import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 
 class CustomDialog(Dialog):
     def __init__(self, parent, title=None):
@@ -9,10 +11,43 @@ class CustomDialog(Dialog):
 
     def body(self, master):
         # 創建對話框主體。返回應具有初始焦點的控件。
-        tk.Label(master, text="請輸入你的名字:").grid(row=0)
-        self.name_entry = tk.Entry(master)
-        self.name_entry.grid(row=0, column=1)
-        return self.name_entry
+        # define columns
+        columns = ('first_name', 'last_name', 'email')
+
+        tree = ttk.Treeview(self, columns=columns, show='headings')
+
+        # define headings
+        tree.heading('first_name', text='First Name')
+        tree.heading('last_name', text='Last Name')
+        tree.heading('email', text='Email')
+
+        # generate sample data
+        contacts = []
+        for n in range(1, 100):
+            contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+
+        # add data to the treeview
+        for contact in contacts:
+            tree.insert('', tk.END, values=contact)
+
+
+        def item_selected(event):
+            for selected_item in tree.selection():
+                item = tree.item(selected_item)
+                record = item['values']
+                # show a message
+                showinfo(title='Information', message=','.join(record))
+
+
+        tree.bind('<<TreeviewSelect>>', item_selected)
+
+        tree.pack(side='left')
+
+        # add a scrollbar
+        scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=tree.yview)
+        tree.configure(yscroll=scrollbar.set)
+        scrollbar.pack(side='left',fill='y')
+        
 
     def apply(self):
         # 當用戶按下確定時處理數據
